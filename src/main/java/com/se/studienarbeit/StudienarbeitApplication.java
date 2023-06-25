3926ee98fb2f7a667c2531c0b132ae61dea9f46f
mrbnr <fherrnbe@hm.edu>
2023-06-25 17:39:30 +0200

    java fix

--- src/main/java/com/se/studienarbeit/StudienarbeitApplication.java
+++ src/main/java/com/se/studienarbeit/StudienarbeitApplication.java
@@ -2,12 +2,20 @@ package com.se.studienarbeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class StudienarbeitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudienarbeitApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void doSomethingAfterStartup() {
+    	MainController.setAwakeStatus(true);
+		System.out.println("Set awake Status to Online");
 	}
 
 }
--- src/main/java/com/se/studienarbeit/UserRepository.java
+++ src/main/java/com/se/studienarbeit/UserRepository.java
@@ -2,8 +2,6 @@ package com.se.studienarbeit;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.se.studienarbeit.User;
-
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
