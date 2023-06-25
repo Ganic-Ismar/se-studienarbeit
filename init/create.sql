d0135f3fa177ba9556b745f89aa323d5e7686e25
Ismar Ganic <ganic@hm.edu>
2023-06-25 15:04:38 +0200

    changed plz varchar

--- init/create.sql
+++ init/create.sql
@@ -2,8 +2,8 @@ create table se_studienarbeit (
 id bigint not null AUTO_INCREMENT,
 name varchar(255) not null,
 email varchar(100) not null,
-password varchar(30) not null,
-plz varchar(6),
+password varchar(32) not null,
+plz varchar(5),
 strasse varchar(100),
 ort varchar(100),
 primary key (id)
